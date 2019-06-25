2e7f3cdc12ecef4632fc3df063dccd9ab69252fa
Jonathan Wong <jonathan.wong110@gmail.com>
2019-06-25 18:51:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,7 +18,7 @@ class Application
         resp.status = 404
       end
     else
-      resp.write "Route not found"
+      resp.write "Item not found"
       resp.status = 404
     end
     resp.finish
