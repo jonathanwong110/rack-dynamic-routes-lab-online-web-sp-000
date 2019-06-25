19453173008e38b3af152a626b38fbc5dc584a70
Jonathan Wong <jonathan.wong110@gmail.com>
2019-06-25 18:45:56 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,32 @@
+class Application
+  
+  @@items = [Item.new("Figs", 3.42), Item.new("Pears", 0.99)]
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path=="/item"
+      resp.write "You requested the songs"
+    elsif req.path.match(/items/)
+      item_price = req.path.split("/items/").last
+      item = @@items.find{|i| i.price == item_price}
+      resp.write @item
+      resp.write 200
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+  
+  def handle_search(search_term)
+    if @@items.include?(search_term)
+      return "#{search_term} is one of our items"
+    else
+      return "Item not found"
+      resp.status
+    end
+  end
+
+end
